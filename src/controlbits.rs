7c452f5afbf39749bd3dc7c1d3d0a5f42eabd699
Bernhard <34011017+Colfenor@users.noreply.github.com>
2021-12-16 01:46:23 +0100

    fix layer iteration & refactor

--- src/controlbits.rs
+++ src/controlbits.rs
@@ -10,12 +10,14 @@ use crate::{int32_sort, params::GFBITS};
 /* input: s, meaning that stride-2^s cswaps are performed */
 /* input: cb, the control bits */
 /* output: the result of apply the control bits to p */
+
+// cb size 8192
 pub fn layer(p: &mut [i16], cb: &[u8], s: i32, n: i32) {
     let stride = 1 << s;
     let mut index = 0;
     let (mut d, mut m): (i16, i16) = (0, 0);
 
-    for i in stride * 2..n {
+    for i in (0..n as usize).step_by(stride * 2) {
         for j in 0..stride {
             d = (p[(i + j) as usize] ^ p[(i + j + stride) as usize]) as i16;
             m = ((cb[index >> 3] >> (index & 7)) & 1) as i16;
@@ -41,7 +43,6 @@ let B = &temp[n as usize..];
 let q = &temp[(n + n / 4) as usize..];
 */
 
-/*
 pub fn cbrecursion(
     out: &mut [u8],
     mut pos: i64,
@@ -49,14 +50,16 @@ pub fn cbrecursion(
     pi: &mut [i16],
     w: i64,
     n: i64,
-    temp: &mut [i32; 2*(1 << GFBITS)],
+    temp: &mut [i32; 2 * (1 << GFBITS)],
 ) {
     assert_eq!(n, 1 << GFBITS);
     //let mut A = alias::slice(temp);
-                                         // use rust aliasing
+    let mut A = temp;
+    let mut B = [0i32; 1 << GFBITS];
+    B.clone_from_slice(&A[n as usize..]);
     //let mut B = alias::slice(temp[n as usize..]);
     //let mut q = alias::slice(temp[(n + n / 4) as usize..]);
-    let (discard, B) = temp.split_at_mut(n as usize);
+    //let (discard, B) = A.split_at_mut(n as usize);
 
     /*let mut B = [0i32; 2 * (1 << GFBITS)];
     let mut temp_q = [0i32; 2 * (1 << GFBITS)];
@@ -74,17 +77,17 @@ pub fn cbrecursion(
     }
 
     for x in 0..n as usize {
-        temp[x] = ((pi[x] ^ 1).overflowing_shl(16).0 | pi[x ^ 1]) as i32;
+        A[x] = ((pi[x] ^ 1).overflowing_shl(16).0 | pi[x ^ 1]) as i32;
     }
 
-    int32_sort(temp, n);
+    int32_sort(A, n);
 
     let mut Ax = 0;
     let mut px = 0;
     let mut cx = 0;
 
     for x in 0..n as usize {
-        Ax = temp[x];
+        Ax = A[x];
         px = Ax & 0xffff;
         cx = px;
         if x < cx as usize {
@@ -94,9 +97,9 @@ pub fn cbrecursion(
     }
 
     for x in 0..n as usize {
-        temp[x] = (temp[x] << 16) | x as i32;
+        A[x] = (A[x] << 16) | x as i32;
     }
-    int32_sort(temp, n);
+    int32_sort(A, n);
 
     let mut ppcpx = 0;
     let mut ppcx = 0;
@@ -104,23 +107,23 @@ pub fn cbrecursion(
 
     if w <= 10 {
         for x in 0..n as usize {
-            B[x] = ((temp[x] & 0xffff) << 10) | (B[x] & 0x3ff);
+            B[x] = ((A[x] & 0xffff) << 10) | (B[x] & 0x3ff);
         }
 
         for i in 1..w - 1 {
             for x in 0..n as usize {
-                temp[x] = ((B[x] & !0x3ff) << 6) | x as i32;
+                A[x] = ((B[x] & !0x3ff) << 6) | x as i32;
             }
-            int32_sort(temp, n);
+            int32_sort(A, n);
 
             for x in 0..n as usize {
-                temp[x] = (temp[x] << 20) | B[x];
+                A[x] = (A[x] << 20) | B[x];
             }
-            int32_sort(temp, n);
+            int32_sort(A, n);
 
             for x in 0..n as usize {
-                ppcpx = temp[x]  & 0xfffff;
-                ppcx = (temp[x]  & 0xffc00) | (B[x] & 0x3ff);
+                ppcpx = A[x] & 0xfffff;
+                ppcx = (A[x] & 0xffc00) | (B[x] & 0x3ff);
 
                 if ppcpx < ppcx {
                     ppcx = ppcpx;
@@ -134,33 +137,33 @@ pub fn cbrecursion(
         }
     } else {
         for x in 0..n as usize {
-            B[x] = (temp[x]  << 16) | (B[x] & 0xffff);
+            B[x] = (A[x] << 16) | (B[x] & 0xffff);
         }
 
         for i in 1..w - 1 {
             for x in 0..n as usize {
-                temp[x] = (B[x] & !0xffff) | x as i32;
+                A[x] = (B[x] & !0xffff) | x as i32;
             }
-            int32_sort(temp, n);
+            int32_sort(A, n);
 
             for x in 0..n as usize {
-                temp[x] = (temp[x]  << 16) | (B[x] & 0xffff);
+                A[x] = (A[x] << 16) | (B[x] & 0xffff);
             }
 
             if i < w - 2 {
                 for x in 0..n as usize {
-                    B[x] = (temp[x]  & !0xffff) | (B[x] >> 16);
+                    B[x] = (A[x] & !0xffff) | (B[x] >> 16);
                 }
-                int32_sort(B, n);
+                int32_sort(&mut B, n);
 
                 for x in 0..n as usize {
-                    B[x] = (B[x] << 16) | (temp[x]  & 0xffff);
+                    B[x] = (B[x] << 16) | (A[x] & 0xffff);
                 }
             }
-            int32_sort(temp, n);
+            int32_sort(A, n);
 
             for x in 0..n as usize {
-                cpx = (B[x] & !0xffff) | (temp[x]  & 0xffff);
+                cpx = (B[x] & !0xffff) | (A[x] & 0xffff);
                 if cpx < B[x] {
                     B[x] = cpx;
                 }
@@ -172,9 +175,9 @@ pub fn cbrecursion(
     }
 
     for x in 0..n as usize {
-        temp[x] = (pi[x].overflowing_shl(16).0 + x as i16) as i32;
+        A[x] = (pi[x].overflowing_shl(16).0 + x as i16) as i32;
     }
-    int32_sort(temp, n);
+    int32_sort(A, n);
 
     let mut x: usize = 0;
     let mut fj: i32 = 0;
@@ -190,10 +193,10 @@ pub fn cbrecursion(
         out[(pos >> 3) as usize] ^= (fj << (pos & 7)) as u8;
         pos += step;
 
-        B[x] = (temp[x]  << 16) | Fx;
-        B[x + 1] = (temp[x + 1]  << 16) | Fx1;
+        B[x] = (A[x] << 16) | Fx;
+        B[x + 1] = (A[x + 1] << 16) | Fx1;
     }
-    int32_sort(B, n);
+    int32_sort(&mut B, n);
 
     pos += (2 * w - 3) * step * (n / 2);
 
@@ -211,10 +214,10 @@ pub fn cbrecursion(
         out[(pos >> 3) as usize] ^= (lk << (pos & 7)) as u8;
         pos += step;
 
-        temp[y] = (Ly << 16) | (B[y] & 0xffff);
-        temp[y + 1] = (Ly1 << 16) | (B[y + 1] & 0xffff);
+        A[y] = (Ly << 16) | (B[y] & 0xffff);
+        A[y + 1] = (Ly1 << 16) | (B[y + 1] & 0xffff);
     }
-    int32_sort(temp, n);
+    int32_sort(A, n);
 
     pos -= (2 * w - 2) * step * (n / 2);
 
@@ -231,7 +234,6 @@ pub fn cbrecursion(
     //cbrecursion(out, pos, step * 2, &mut q, w - 1, n / 2, temp);
     //cbrecursion(out, pos + step, step * 2, &mut new_q, w - 1, n / 2, temp);
 }
-*/
 
 // params
 // out = sk secret key bits
@@ -253,41 +255,28 @@ pub fn controlbitsfrompermutation(out: &mut [u8], pi: &mut [i16], w: i64, n: i64
 
     8192 >> 4 = 512, stepsize
     */
+    out.fill(0);
+    let mut ptr = out; // moving value to ptr
+    let mut first_itr = true;
+
     loop {
-        counter = 0;
-
-        for chunk in out.chunks_mut(1) {
-            for elem in chunk.iter_mut() {
-                *elem = 0;
-            }
-            counter += 1;
-            if counter == 12800 {
-                break;
-            }
-        }
-        //cbrecursion(out, 0, 1, pi, w, n, &mut temp);
-
-        /*for i in 0..n as usize {
-            pi_test[i] = i as i16;
-        }*/
-
-        let mut ptr = out.to_vec();
-        counter = 0;
-        for chunk in ptr.chunks_mut((n >> 4) as usize) {
-            if counter == w as i32 {
-                break;
-            }
-            layer(&mut pi_test, chunk, counter, n as i32);
-            counter += 1;
+        if first_itr == true {
+            first_itr = false;
+        } else {
+            ptr.fill(0);
         }
 
-        counter = (w - 2) as i32;
-        for chunk in ptr.chunks_mut((n >> 4) as usize) {
-            if counter == 0 {
-                break;
-            }
-            layer(&mut pi_test, chunk, counter, n as i32);
-            counter -= 1;
+        cbrecursion(ptr, 0, 1, pi, w, n, &mut temp);
+
+        //let mut ptr = out; // moving value to ptr
+        for i in 0..w as usize {
+            layer(&mut pi_test, ptr, i as i32, n as i32);
+            ptr = &mut ptr[(n >> 4) as usize..];
+        }
+
+        for i in (0..w - 2).rev() {
+            layer(&mut pi_test, ptr, i as i32, n as i32);
+            ptr = &mut ptr[(n >> 4) as usize..];
         }
 
         for i in 0..n as usize {
