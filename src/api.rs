acdff9220f4dd1fde004e08e87ab458ee6dc10f7
Lukas Prokop <lukas.prokop@iaik.tugraz.at>
2022-01-27 23:58:29 +0100

    Introduce KAT KEM tests.

--- src/api.rs
+++ src/api.rs
@@ -2,3 +2,4 @@ pub const CRYPTO_PUBLICKEYBYTES: usize = 1357824;
 pub const CRYPTO_SECRETKEYBYTES: usize = 14120;
 pub const CRYPTO_CIPHERTEXTBYTES: usize = 240;
 pub const CRYPTO_BYTES: usize = 32;
+pub const CRYPTO_PRIMITIVE: &str = "mceliece348864";
