4e50d424a959385eaeb02251900abb3e420dc542
prokls <lukas.prokop@student.tugraz.at>
2022-08-21 14:51:31 +0200

    Use digest_xof, not finalize_xof_into for SHAKE256
    This was discussed in PR #17 and was taken over from branch v2

--- src/crypto_hash.rs
+++ src/crypto_hash.rs
@@ -1,13 +1,11 @@
 //! Hash function implementations (only SHAKE)
 
-use sha3::digest::{ExtendableOutput, Update};
+use sha3::digest::ExtendableOutput;
 use sha3::Shake256;
 
 /// Utilizes the SHAKE256 hash function. Input and output is of arbitrary length.
 pub(crate) fn shake256(output: &mut [u8], input: &[u8]) {
-    let mut shake_hash_fn = Shake256::default();
-    shake_hash_fn.update(input);
-    shake_hash_fn.finalize_xof_into(output);
+    Shake256::digest_xof(input, output);
 }
 
 #[cfg(test)]
