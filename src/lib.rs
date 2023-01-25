8e843213a92776885e63fa9cf862f8d08c0e83cc
Linus Färnstrand <faern@faern.net>
2023-01-25 10:43:33 +0100

    Add tests for EncappedKey and EncappedKeySize

--- src/lib.rs
+++ src/lib.rs
@@ -531,7 +531,9 @@ mod kem_api {
 
     #[cfg(test)]
     mod tests {
-        use kem::{Decapsulator, Encapsulator};
+        use crate::{Ciphertext, CRYPTO_CIPHERTEXTBYTES};
+        use kem::generic_array::GenericArray;
+        use kem::{Decapsulator, EncappedKey, Encapsulator};
 
         #[test]
         fn test_crypto_kem_api() -> Result<(), kem::Error> {
@@ -548,6 +550,27 @@ mod kem_api {
             assert_eq!(shared_secret.as_bytes(), shared_secret2.as_bytes());
 
             Ok(())
+        }
+
+        #[test]
+        fn ciphertext_generic_array_length() {
+            let ciphertext =
+                GenericArray::<u8, <Ciphertext as EncappedKey>::EncappedKeySize>::default();
+            assert_eq!(ciphertext.len(), CRYPTO_CIPHERTEXTBYTES);
+        }
+
+        #[test]
+        fn ciphertext_from_bytes() {
+            let mut key_material = GenericArray::<_, _>::default();
+            for (i, byte) in key_material.iter_mut().enumerate() {
+                *byte = i as u8;
+            }
+            let ciphertext = <Ciphertext as EncappedKey>::from_bytes(&key_material).unwrap();
+
+            // Verify that the ciphertext contains CRYPTO_CIPHERTEXTBYTES bytes and all with the correct value
+            for i in 0..CRYPTO_CIPHERTEXTBYTES {
+                assert_eq!(ciphertext.as_array()[i], i as u8)
+            }
         }
     }
 }
