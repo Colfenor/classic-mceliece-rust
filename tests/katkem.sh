e1a312d08699d77d67f2609958b0ca87db3eb027
Bernhard <34011017+Colfenor@users.noreply.github.com>
2023-03-05 01:23:01 +0100

    fix katkem.sh compilation feature flag & refactor to run_katkem

--- tests/katkem.sh
+++ tests/katkem.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 declare -A variants
-variants["mceliece348864"]="187255d64e8139fe729dce3851f05ba3"
-variants["mceliece348864f"]="1f44974b7f792f31dc702e57372d1d0e"
-variants["mceliece460896"]="5734a1bfa0978d51d226c1cfb1927626"
-variants["mceliece460896f"]="f0e45c122c6af7fb71907e237f61b9e1"
-variants["mceliece6688128"]="cd8d87674bfa9dac99295645ada4b4e5"
-variants["mceliece6688128f"]="0fe9b57183ed7a9f8696c87b199972a0"
-variants["mceliece6960119"]="5dcd52afbd4e8f4828d6d5fe3bc6f873"
-variants["mceliece6960119f"]="2d0d8634eae40e009ca5deffe167d912"
-variants["mceliece8192128"]="3adb51bd37e72d4fb47ef128505e2e53"
-variants["mceliece8192128f"]="f295355034e5ae6b1274aaf7c0a36730"
+variants["mceliece348864"]="f932d4f75d1a788ad58e7d20af8defe9"
+variants["mceliece348864f"]="70e10264d735abe77a509d853bfc6f6d"
+variants["mceliece460896"]="7d2d60f492a8e74a33696a0616f61746"
+variants["mceliece460896f"]="5ce8c2ecbb8c94082b475ff090f457c4"
+variants["mceliece6688128"]="e7ad02c431ac9019820b7ce96654b240"
+variants["mceliece6688128f"]="39984724cdabb810cdc76ade08a9bf52"
+variants["mceliece6960119"]="819e4a4748f201e47d70f28f5b639303"
+variants["mceliece6960119f"]="59426af22ec3a5e5dddc0969782832a6"
+variants["mceliece8192128"]="9dc71f8a9f8a6492e2b7c341b8a0801b"
+variants["mceliece8192128f"]="8022c8ffd8d938e56840261c91d1e59a"
 
 RET=0
-TMPDIR=$(mktemp -d) 
-for var in "${!variants[@]}" 
+TMPDIR=$(mktemp -d)
+for var in "${!variants[@]}"
 do
-    cargo test --release --features "$var" --package classic-mceliece-rust --lib -- tests::test_katkem $TMPDIR/$var.req $TMPDIR/$var.rsp
+    cargo test --release --features "$var kem" --package classic-mceliece-rust --lib -- tests::test_katkem $TMPDIR/$var.req $TMPDIR/$var.rsp
     MD5HASH=$(md5sum ${TMPDIR}/${var}.rsp | awk '{print $1}')
     if [[ "$MD5HASH" != "${variants[$var]}" ]]; then
         echo "KAT not as expected for ${var}."
